731f54f46a1a9f8a5387fb8557e63e7dd37a2e41
John Kealy <johnckealy@gmail.com>
2020-06-11 16:00:26 +0100

    adjusted base dir for articles

--- scripts/seed_articles.py
+++ scripts/seed_articles.py
@@ -1,13 +1,17 @@
 from glob import glob
 from skewtapp.models import Article
+from django.conf import settings
 
-MARKDOWN_DIR = "/home/jokea/code/skewt/skewtfrontend/skewtapp/markdown/"
+
+MARKDOWN_DIR = f"{settings.BASE_DIR}/skewtapp/markdown/"
 
 
 def run():
     Article.objects.all().delete()
+    print("Seeding Articles...")
     for md_filename in glob(MARKDOWN_DIR + "*.md"):
         print(md_filename)
         article = Article()
         article.seed_article(md_filename)
+    print("Ok.")
 
